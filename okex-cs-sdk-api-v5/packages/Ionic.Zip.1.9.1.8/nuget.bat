0bd6290c79cd34f5240e3af3f82db6fae45fbcb4
jane <ai747957633@qq.com>
2020-12-23 23:32:15 +0800

    remove cs

--- okex-cs-sdk-api-v5/packages/Ionic.Zip.1.9.1.8/nuget.bat
+++
@@ -1,4 +0,0 @@
-@echo Uploading to nuget
-erase *.nupkg /Q /F
-for /f "delims=|" %%f in ('dir /b *.nuspec') do ..\.nuget\NuGet.exe pack %%f -build -properties Configuration=Release
-..\.nuget\Nuget.exe push *.nupkg
