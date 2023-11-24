b06a872ec946e94bae712fcfcd763b0cdca74a09
1834155205@qq.com <1834155205@qq.com>
2023-11-24 13:41:57 +0800

    11.22update

---
+++ okx-python-sdk-api-v5/okx/Affiliate_api.py
@@ -0,0 +1,8 @@
+from .client import Client
+from .consts import *
+
+
+class AffiliateAPI(Client):
+
+    def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, flag='1'):
+        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, flag)
