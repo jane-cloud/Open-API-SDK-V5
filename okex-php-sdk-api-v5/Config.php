5363eef25e69921ba16253a50f10e49cd1f8dd77
php <>
2023-11-30 16:05:03 +0800

    php

--- okex-php-sdk-api-v5/Config.php
+++ okex-php-sdk-api-v5/Config.php
@@ -13,11 +13,11 @@ Config::$debug=0;
 
 Config::$config=[
 
-//        "apiKey"=>"ce25cb5e-d9a6-48f4-9b99-f5fd30726032",
-//        "apiSecret"=>"3991B5A72B37052F4B03DA7B669B5F47",
-//        "passphrase"=>"Sgl1997.",
-//    //    是否为模拟盘
-//        "paper"=>1
+       "apiKey"=>"ce25cb5e-d9a6-48f4-9b99-f5fd30726032",
+       "apiSecret"=>"3991B5A72B37052F4B03DA7B669B5F47",
+       "passphrase"=>"Sgl1997.",
+   //    是否为模拟盘
+       "paper"=>1
     // "apiKey"=>"a8d457ac-ae55-4836-b2ff-0406f110041d",
     // "apiSecret"=>"A64DF9A671D5D471CB0BDEE1DCF909F6",
     // "passphrase"=>"Sgl1997.",
